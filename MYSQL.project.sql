2cc1341867fa5f5ca128f3d8342c7b9e1f3f6f13
Shukhrat-dev252 <abdurasulovshuhat@gmail.com>
2026-03-04 20:38:30 +0600

    Add files via upload

---
+++ MYSQL.project.sql
@@ -0,0 +1,36 @@
+SELECT * FROM `covid-19`.covid_19;
+select Country,WHO_region,New_deaths,Cumulative_cases
+from `covid-19`.covid_19;
+RENAME TABLE `covid-19`.covid_19 TO covid_19;
+select * from covid_19;
+select Country,WHO_region,New_deaths,Cumulative_cases
+from `covid-19`.covid_19;
+select * from covid_19;
+
+
+# Here died people
+select Country,WHO_region,sum(New_deaths),sum(Cumulative_deaths),(sum(New_deaths)/sum(Cumulative_deaths)*100)as died_infliktion
+from covid_19
+group by Country,WHO_region;
+
+
+select * from covid_19;
+
+
+
+
+select Country,WHO_region,sum(New_cases),sum(Cumulative_cases),(sum(New_cases)/sum(Cumulative_cases)*100) as percent_cases
+from covid_19
+group by country,WHO_region;
+
+
+
+
+select Country,WHO_region,sum(Cumulative_cases)
+from covid_19
+group by Country, WHO_region;
+
+
+
+
+
